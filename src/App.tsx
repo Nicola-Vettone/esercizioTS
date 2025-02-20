5b23d52d2f48a92a9927874ff430e405b9fcfa04
Nicola Vettone <nicolavettone57@gmail.com>
2025-02-20 17:00:00 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import AllArticle from "./components/AllArticle";
+import TopBar from "./components/TopBar";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/article/:id" element={<AllArticle />}></Route>
-        </Routes>
+        <Container fluid>
+          <TopBar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/article/:id" element={<AllArticle />}></Route>
+          </Routes>
+        </Container>
       </BrowserRouter>
     </>
   );
